b64c7112e6f06c6dccec643f71a83b54094b4179
mincho8050 <mincho8050@gmail.com>
2019-09-18 20:35:48 +0900

    20190918:애니메이션미적용(연습만),옵션메뉴적용

--- app/src/main/java/com/example/baseballgame/MainActivity.kt
+++ app/src/main/java/com/example/baseballgame/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.baseballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.widget.ToggleButton
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.email
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.webView
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +26,26 @@ class MainActivity : AppCompatActivity() {
 
     }//onCreate
 
+    /*위에 상단 옵션메뉴 보이기*/
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.infor_copyright ->{
+                return true
+            }
+            R.id.infor_developer ->{
+                email("mincho8050@gmail.com")
+                return true
+            }
+        }//when
+        return super.onOptionsItemSelected(item)
+    }//onOptionsItemSelected
+
 
 
 }//
+
